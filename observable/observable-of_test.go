38c83b4b2ce799136fe4ebe0937e3573551712e0
RainInFall <lygjcsrq@163.com>
2017-09-09 00:24:42 +0800

    fix coverage

--- observable/observable-of_test.go
+++ observable/observable-of_test.go
@@ -28,3 +28,29 @@ func TestObservableOf(t *testing.T) {
 
 	assert.Exactly([]int{1, 2, 3}, values)
 }
+
+func TestObservableOfUnsubscribe(t *testing.T) {
+	assert := assert.New(t)
+
+	oba := Of(1, 2, 3, errors.New("Bang"), 4, nil)
+	obs := observer.New(0)
+	oba.Subscribe(obs)
+
+	values := []int{}
+	i := 0
+	for v := range obs.Out() {
+		switch v := v.(type) {
+		case error:
+			assert.Exactly(errors.New("Bang"), v)
+			continue
+		default:
+			values = append(values, v.(int))
+		}
+		i++
+		if i == 3 {
+			obs.Unsubscribe()
+		}
+	}
+
+	assert.Exactly([]int{1, 2, 3}, values)
+}
