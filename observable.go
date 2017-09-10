69e329bc88a1bd02e7d42a19933d648bdb0cb742
RainInFall <lygjcsrq@163.com>
2017-09-10 13:36:48 +0800

    fix git commit

--- observable.go
+++ observable.go
@@ -3,4 +3,8 @@ package rx
 /*Observable is a representation of any set of values over any amount of time*/
 type Observable interface {
 	Subscribe(InObserver)
+	Map(MapFunc) Observable
 }
+
+/*MapFunc used by Map*/
+type MapFunc func(interface{}) interface{}
